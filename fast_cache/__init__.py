c67ce878319a4bf7a75381f35c4deeb04d66948f
Bijay Nayak <bijay6779@gmail.com>
2025-06-12 19:23:34 +0530

    Fix Imports

--- fast_cache/__init__.py
+++ fast_cache/__init__.py
@@ -2,8 +2,9 @@ from .integration import FastAPICache
 from .backends.backend import CacheBackend
 
 from .backends.redis import RedisBackend
+from .backends.memory import InMemoryBackend
 
-__all__ = ["FastAPICache", "RedisBackend", "CacheBackend", "cache"]
+__all__ = ["FastAPICache", "RedisBackend", "CacheBackend","InMemoryBackend", "cache"]
 
 
 # Create global cache instance
