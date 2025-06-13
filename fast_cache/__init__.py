f16b47120f3d1107b95ec19106fafb8af79ba553
Bijay Nayak <bijay6779@gmail.com>
2025-06-13 15:39:42 +0530

    Add Postgres Backend

--- fast_cache/__init__.py
+++ fast_cache/__init__.py
@@ -3,8 +3,9 @@ from .backends.backend import CacheBackend
 
 from .backends.redis import RedisBackend
 from .backends.memory import InMemoryBackend
+from .backends.postgres import PostgresBackend
 
-__all__ = ["FastAPICache", "RedisBackend", "CacheBackend", "InMemoryBackend", "cache"]
+__all__ = ["FastAPICache", "RedisBackend", "CacheBackend", "InMemoryBackend","PostgresBackend", "cache"]
 
 
 # Create global cache instance
